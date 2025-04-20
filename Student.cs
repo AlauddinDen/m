4f119706d745fa6dfbaabceb68c111bf15a766d0
AlauddinDen <alauddinarif1997@gmail.com>
2025-04-20 13:15:13 +0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public bool IsActive { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<Address> Addresses { get; set; }=new List<Address>();
+
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int StudentId { get; set; }
+
+    }
+}
---
+++ StudentDto.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public bool IsActive { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public string AddressJson { get; set; }
+        public string BaseImage64 { get; set; }
+
+
+    }
+    public class AddressDto
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+
+    }
+}
